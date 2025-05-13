cb01c8223eb3a2e98b5a434feca81aa08ef71ead
Carlos Camacho <carloscamacho0602@gmail.com>
2025-05-12 23:57:01 -0500

    Se añaden modelos para Universidad, Contacto, ProgramaAcademico, y Admision con registro de administrador.

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Universidad, ProgramaAcademico, Contacto, Admision
 
 # Register your models here.
+admin.site.register(Universidad)
+admin.site.register(ProgramaAcademico)
+admin.site.register(Contacto)
+admin.site.register(Admision)
--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
 # Create your models here.
+class Universidad(models.Model):
+    id_universidad = models.AutoField(primary_key=True)
+    nombre_universidad = models.CharField(max_length=100)
+    ciudad = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'universidad'
+    
+    def __str__(self):
+        return self.nombre_universidad
+    
+class Contacto(models.Model):
+    id_contacto = models.AutoField(primary_key=True)
+    id_universidad = models.ForeignKey(Universidad, on_delete=models.CASCADE, db_column='id_universidad')
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+    correo = models.EmailField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'contacto'
+    
+    def __str__(self):
+        return f'Contacto de {self.id_universidad.nombre_universidad} - {self.telefono}'
+
+
+class ProgramaAcademico(models.Model):
+    codigo_snies = models.AutoField(primary_key=True, max_length=15)
+    id_universidad = models.ForeignKey(Universidad, on_delete=models.CASCADE, db_column='id_universidad')
+    nombre_programa = models.CharField(max_length=100)
+    duracion = models.CharField(max_length=50, blank=True, null=True)
+    numero_cupos = models.IntegerField(blank=True, null=True)
+    periocidad_admisiones = models.CharField(max_length=50, blank=True, null=True)
+    url = models.URLField(max_length=200, blank=True, null=True)
+    titulo_otorgado = models.CharField(max_length=100, blank=True, null=True)
+    total_creditos = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'programa_academico'
+    
+    def __str__(self):
+        return f'{self.nombre_programa} - {self.id_universidad.nombre_universidad}'
+    
+
+class Admision(models.Model):
+    id_admision = models.CharField(primary_key=True, max_length=50)
+    codigo_snies = models.ForeignKey(ProgramaAcademico, on_delete=models.CASCADE, db_column='codigo_snies')
+    periodo = models.CharField(max_length=6)
+    fecha_inicio_inscripcion = models.DateField(blank=True, null=True)
+    fecha_cierre_inscripcion = models.DateField(blank=True, null=True)
+    fecha_examen_admision = models.DateField(blank=True, null=True)
+    fecha_publicacion_admitidos = models.DateField(blank=True, null=True)
+    paso_a_paso = models.CharField(max_length=200, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'admision'
+    
+    def __str__(self):
+        return f'Admisión {self.codigo_snies.nombre_programa} - {self.fecha_inicio_inscripcion} a {self.fecha_cierre_inscripcion}'
