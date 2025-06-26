74c4e89ef10668a5eaa1d6409e616b6b99e0362d
Carlos Camacho <carloscamacho0602@gmail.com>
2025-06-25 21:14:40 -0500

    Refactorización de las vistas para incluir el registro de usuarios y los detalles del programa; mejora de la lógica de listado de programas.

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,16 +1,17 @@
-from django.shortcuts import render
-from .models import ProgramaAcademico, Universidad
-
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
+from .models import ProgramaAcademico, Universidad, Admision
 
 
 # Create your views here.
 def lista_programas(request):
 
-    busqueda = request.GET.get('busqueda') if request.GET.get('busqueda') else ''
-    especialidad_id = request.GET.get('especialidad_id')
-    especialidad_id = int(especialidad_id) if especialidad_id else ''
-    id_universidad = request.GET.get('id_universidad')
-    id_universidad = int(id_universidad) if id_universidad else ''
+    busqueda = request.GET.get("busqueda") if request.GET.get("busqueda") else ""
+    especialidad_id = request.GET.get("especialidad_id")
+    especialidad_id = int(especialidad_id) if especialidad_id else ""
+    id_universidad = request.GET.get("id_universidad")
+    id_universidad = int(id_universidad) if id_universidad else ""
 
     programas_academicos = ProgramaAcademico.objects
 
@@ -24,12 +25,47 @@ def lista_programas(request):
             id_universidad=id_universidad
         )
 
-    programas_academicos = programas_academicos.all()
+    programas_academicos = (
+        programas_academicos.exclude(codigo_snies__in=["N/A", "", "No disponible"])
+        .exclude(codigo_snies__isnull=True)
+        .all()
+    )
+
+    # programas_academicos = programas_academicos.all()
+
+    print(programas_academicos)
 
     universidades = Universidad.objects.all()
 
-    return render(request, 'lista_programas.html',
-                  {'programas_academicos': programas_academicos,
-                   'busqueda': busqueda,
-                   'id_universidad': id_universidad,
-                   'universidades': universidades})
+    return render(
+        request,
+        "lista_programas.html",
+        {
+            "programas_academicos": programas_academicos,
+            "busqueda": busqueda,
+            "id_universidad": id_universidad,
+            "universidades": universidades,
+        },
+    )
+
+
+def detalle_programa(request, codigo_snies):
+    # programa_academico = ProgramaAcademico.objects.get(codigo_snies=codigo_snies)
+    programa_academico = get_object_or_404(ProgramaAcademico, codigo_snies=codigo_snies)
+    # admision = get_object_or_404(Admision, codigo_snies=programa_academico.codigo_snies)
+    admision = Admision.objects.filter(
+        codigo_snies=programa_academico.codigo_snies
+    ).first()
+
+    print(programa_academico)
+    print(admision)
+
+    return render(
+        request,
+        "detalle_programa.html",
+        {"programa_academico": programa_academico, "admision": admision},
+    )
+
+
+def registro_usuario(request):
+    return render(request, "signup.html", {"form": CustomUserCreationForm()})
